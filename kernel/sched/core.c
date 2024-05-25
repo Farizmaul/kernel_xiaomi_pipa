a9d35b1715f9603a5c1b7a42972b41c3ec7e405a
Yuan Si <do4suki@gmail.com>
2024-05-25 14:36:04 +0800

    fixup! kernel/sched: Cover more walt cases
    Change-Id: I11d6a154d1b2a01ca9962357d34c2035c4791c85
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4220,7 +4220,9 @@ void scheduler_tick(void)
 	u64 wallclock;
 	bool early_notif;
 	u32 old_load;
+#ifdef CONFIG_SCHED_WALT
 	struct related_thread_group *grp;
+#endif
 	unsigned int flag = 0;
 
 	sched_clock_tick();
